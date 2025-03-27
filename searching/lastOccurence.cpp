fba84079e7917ea26b3d281b8fa0c4b501a3cb3c
sunnyrajendraraj <sunny.2201108cs@iiitbh.ac.in>
2025-03-28 02:08:15 +0530

    first & last occurence updated

--- searching/lastOccurence.cpp
+++ searching/lastOccurence.cpp
@@ -1,21 +1,21 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int firstOccurence(vector<int> &nums, int x){
+int lastOccurence(vector<int> &nums, int x){
     int low=0, high=nums.size()-1;
-    int first=-1;
+    int last=-1;
     while(low<=high){
         int mid=low+(high-low)/2;
         if(nums[mid]==x){
-            first=mid;
-            high=mid-1;
+            last=mid;
+            low=mid+1;
         }else if(nums[mid]<x){
             low=mid+1;
         }else{
             high=mid-1;
         }
     }
-    return first;
+    return last;
 }
 
 int main(){
@@ -32,6 +32,6 @@ int main(){
         cout<<nums[i]<<" ";
     }
     cout<<endl;
-    int fo = firstOccurence(nums,x);
-    cout<<"First Occurence of "<<x<<" is "<<fo<<endl;
+    int lo = lastOccurence(nums,x);
+    cout<<"Last Occurence of "<<x<<" is "<<lo<<endl;
 }
